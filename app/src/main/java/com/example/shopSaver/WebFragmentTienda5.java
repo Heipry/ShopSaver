cb2cef4ffec855c6c22b1d28bc925f0035a42d34
Heipry <heipry@hotmail.com>
2025-05-21 09:46:18 +0200

    Added swipe refresh to fragment 5(experimental), cookie error on lupa solved

--- app/src/main/java/com/example/shopSaver/WebFragmentTienda5.java
+++ app/src/main/java/com/example/shopSaver/WebFragmentTienda5.java
@@ -13,7 +13,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
-import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
